0bca784fe84d933013ce86115aec00ed464987d9
tanishka1306 <taniskha.rajpoot_cs20@gla.ac.in>
2021-11-11 23:20:18 +0530

    practice problem

--- Employee.java
+++ Employee.java
@@ -1,3 +1,11 @@
+
+/* Write a program by creating an 'Employee' class having the following methods and 
+print the final salary.
+
+1 - 'getInfo()' which takes the salary, number of hours of work per day of the employee as parameter
+2 - 'AddSal()' which adds $10 to the salary of the employee if it is less than $500.
+3 - 'AddWork()' which adds $5 to the salary of the employee if the number of hours of
+     work per day is more than 6 hours.*/
 import java.util.*;
 
 class EmployeeDetail {
