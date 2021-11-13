33e63ab771f617a06a5e26e57e992f302698e5cc
tanishka1306 <taniskha.rajpoot_cs20@gla.ac.in>
2021-11-13 18:41:35 +0530

    command line argument practice question

---
+++ src/Sample.java
@@ -0,0 +1,21 @@
+/*Write a Program to accept a String as a command line argument
+and print a Welcome message as given below.
+
+        Example1)
+        C:\> java Sample Aman
+        O/P Expected : Welcome Aman
+        Note: save this file with Sample.java
+
+
+ */
+
+class Sample {
+
+    public static void main(String args[]) // main  method
+
+    {
+
+        System.out.println("Welcome " + args[0]); // print
+
+    }
+}
