1dfd7a69c772c282adc32fefc91b0933e8b70bfe
tanishka1306 <taniskha.rajpoot_cs20@gla.ac.in>
2021-11-13 13:21:16 +0530

    practice question

---
+++ src/student_name.java
@@ -0,0 +1,25 @@
+/*Write a program to print the names of students by creating a Student class.
+If no name is passed while creating an object of Student class,
+then the name should be "Unknown", otherwise the name should be equal to the String value
+passed while creating object of Student class.*/
+class student{
+    String ss;
+    String name;
+    public student(String ss){
+        name = ss;
+    }
+    public student()
+    {
+        name = "unknown";
+    }
+}
+public class student_name{
+    public static void main(String[] args) {
+        student obj = new student();
+        obj.ss = "Vivek";
+        //Student ss1 = new Student();
+        System.out.println(obj.ss);
+        System.out.println(obj.name);
+
+    }
+}
